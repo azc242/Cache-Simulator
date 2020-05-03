e1c33215284d42ee22da8c1304d6ef224b1ad7d4
Alan Zongyi Chen <azc242@access2.cims.nyu.edu>
2020-05-03 16:37:32 -0400

    updated csim

--- csim.c
+++ csim.c
@@ -1,5 +1,19 @@
 #include "lab3.h"
 
+//Name: Alan Chen
+//UserID: azc242
+
+// cache parameters
+typedef struct{
+    int s; // 2^s cache sets
+    int S; // number of sets
+    int E; // cache lines in a set
+    int b; // parameter used for getting B
+    int B; // B = 2^b (bytes per cache block)
+    char *traceFile; // valgrind trace file
+} cacheParam;
+
+
 int main()
 {
     printSummary(0, 0, 0);
